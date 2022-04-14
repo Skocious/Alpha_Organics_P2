b548bb5200687c9e77a04d7d006b1524d1e90933
Samantha WathenOlson <samantha.wathen269@gmail.com>
2022-04-14 11:27:50 -0400

    Delete ProducerDal.java

--- src/Samantha/Dal/ProducerDal.java
+++
@@ -1,9 +0,0 @@
-package src.Samantha.Dal;
-
-import src.Samantha.Entities.Producer;
-
-public interface ProducerDal {
-
-    Producer selectProducerId(int ProducerId, String ProducerName, int LoginId);
-
-}
