ab2d24fba7832d006713f68d6b898dfe5bf1bb72
William Blair <51091296+Skocious@users.noreply.github.com>
2022-04-14 13:54:29 -0500

    Create CustomerImp.java

---
+++ src/main/java/DataAccessLayer/CustomerImp.java
@@ -0,0 +1,31 @@
+package DataAccessLayer;
+import entities.Customer;
+import java.sql.*;
+import utilities.DataBaseConnection;
+
+public class CustomerImp implements CustomerDAO {
+
+    public CustomerImp() {
+        super();
+    }
+
+    @Override
+    public Customer getCustomerId(int customerId) {
+        try (Connection connection = DataBaseConnection.createConnection()) {
+            String sql = "select * from customers where customer_id = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, customerId);
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            Customer customer = new Customer(
+                    rs.getInt("customer_id"),
+                    rs.getString("customer_name"),
+                    rs.getInt("login_id")
+            );
+            return customer;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
