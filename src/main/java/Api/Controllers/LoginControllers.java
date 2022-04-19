e0f8e9452c86845de69fc3ece9c4b05f034de015
William Blair <blaircorporate@gmail.com>
2022-04-19 16:08:22 -0500

    Uncommented out API layer

--- src/main/java/Api/Controllers/LoginControllers.java
+++ src/main/java/Api/Controllers/LoginControllers.java
@@ -1,17 +1,17 @@
-//package Api.Controllers;
-//
-//import DataAccessLayer.*;
-//import io.javalin.core.util.Header;
-//
-//public class LoginControllers {
-//
-//    public Header selectProducerId = ctx ->{
-//        ctx.result(LoginImp.Login);
-//        ctx.result(200);
-//    };
-//
-//    public Header selectCustomerId = ctx ->{
-//        ctx.result(LoginImp.Login);
-//        ctx.result(200);
-//    };
-//}
+package Api.Controllers;
+
+import DataAccessLayer.*;
+import io.javalin.core.util.Header;
+
+public class LoginControllers {
+
+    public Header selectProducerId = ctx ->{
+        ctx.result(LoginImp.Login);
+        ctx.result(200);
+    };
+
+    public Header selectCustomerId = ctx ->{
+        ctx.result(LoginImp.Login);
+        ctx.result(200);
+    };
+}
