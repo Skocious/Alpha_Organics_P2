5c76a000d721a4ab97b977950f8b0c94776bb7bc
unknown <jjacquet1>
2022-04-20 15:37:53 -0400

    minimum required tests

--- src/main/java/Api/Apps.java
+++ src/main/java/Api/Apps.java
@@ -1,53 +1,53 @@
-//package Api;
-//
-//import Api.Controllers.*;
-//import DataAccessLayer.*;
-//import ServiceAccessLayer.*;
-//import io.javalin.Javalin;
-//
-//public class Apps {
-//    public static void main(String[] args){
-//        Javalin app = Javalin.create(config ->{
-//            config.enableCorsForAllOrigins();
-//            config.enableDevLogging();
-//        });
-//
-//        ProducerDAO producerDAO = new ProducerImp();
-//        ProducerSO producerSO = new ProducerSImp(producerDAO);
-//        ProducerControllers producerControllers = new ProducerControllers(producerSO);
-//
-//        LoginDAO loginDAO = new LoginImp();
-//        LoginSO loginSO = new LoginSImp(loginDAO);
-//        LoginControllers loginControllers = new LoginControllers(loginSO);
-//
-//        ItemsDAO itemsDAO = new ItemsImp();
-//        ItemsSO itemsSO = new ItemsSImp(itemsDAO);
-//        ItemsController itemsController = new ItemsController(itemsSO);
-//
-//        TransactionDAO transactionDAO = new TransactionImp();
-//        TransactionsSO transactionsSO = new TransactionSImp(transactionDAO);
-//        TransactionsController transactionsController = new TransactionsController(transactionsSO);
-//
-//        app.post("/items", itemsController.createItems);
-//
-//        app.get("/items/{id}", itemsController.selectItemsById);
-//
-//        app.get("/items", itemsController.selectAllItems);
-//
-//        app.put("/items", itemsController.updateItemsById);
-//
-//        app.delete("/items/{id}", itemsController.deleteItemsById);
-//
-//        app.post("/producer", loginControllers.selectProducerId);
-//
-//        app.post("/customer", LoginControllers.selectCustomerId);
-//
-//        app.post("/transactions", transactionsController.createTransaction);
-//
-//        app.get("/transactions/{producerId}", transactionsController.getAllTransactionByCustomerId);
-//
-//        app.get("/transactions/{customerId}", transactionsController.getAllTransactionsByProducerId);
-//
-//        app.get("/producer", producerControllers.getProducerId);
-//    }
-//}
+package Api;
+
+import Api.Controllers.*;
+import DataAccessLayer.*;
+import ServiceAccessLayer.*;
+import io.javalin.Javalin;
+
+public class Apps {
+    public static void main(String[] args){
+        Javalin app = Javalin.create(config ->{
+            config.enableCorsForAllOrigins();
+            config.enableDevLogging();
+        });
+
+        ProducerDAO producerDAO = new ProducerImp();
+        ProducerSO producerSO = new ProducerSImp(producerDAO);
+        ProducerControllers producerControllers = new ProducerControllers(producerSO);
+
+        LoginDAO loginDAO = new LoginImp();
+        LoginSO loginSO = new LoginSImp(loginDAO);
+        LoginControllers loginControllers = new LoginControllers(loginSO);
+
+        ItemsDAO itemsDAO = new ItemsImp();
+        ItemsSO itemsSO = new ItemsSImp(itemsDAO);
+        ItemsController itemsController = new ItemsController(itemsSO);
+
+        TransactionDAO transactionDAO = new TransactionImp();
+        TransactionsSO transactionsSO = new TransactionSImp(transactionDAO);
+        TransactionsController transactionsController = new TransactionsController(transactionsSO);
+
+        app.post("/items", itemsController.createItems);
+
+        app.get("/items/{id}", itemsController.selectItemsById);
+
+        app.get("/items", itemsController.selectAllItems);
+
+        app.put("/items", itemsController.updateItemsById);
+
+        app.delete("/items/{id}", itemsController.deleteItemsById);
+
+        app.post("/producer", loginControllers.selectProducerId);
+
+        app.post("/customer", LoginControllers.selectCustomerId);
+
+        app.post("/transactions", transactionsController.createTransaction);
+
+        app.get("/transactions/{producerId}", transactionsController.getAllTransactionByCustomerId);
+
+        app.get("/transactions/{customerId}", transactionsController.getAllTransactionsByProducerId);
+
+        app.get("/producer", producerControllers.getProducerId);
+    }
+}
--- src/main/java/Api/Controllers/TransactionsController.java
+++ src/main/java/Api/Controllers/TransactionsController.java
@@ -1,20 +1,20 @@
-//package Api.Controllers;
-//
-//import io.javalin.core.util.Header;
-//
-//public class TransactionsController {
-//    public Header createTransaction = ctx ->{
-//        ctx.result(1, 1, 1, 3.00, 1);
-//        ctx.result(200);
-//    };
-//
-//    public Header getAllTransactionByCustomerId = ctx->{
-//        ctx.result(1, 1, 1, 3.00, 1);
-//        ctx.result(200);
-//    };
-//
-//    public Header getAllTransactionsByProducerId = ctx->{
-//        ctx.result(1, 1, 1, 3.00, 10);
-//        ctx.result(200);
-//    };
-//}
+package Api.Controllers;
+
+import io.javalin.core.util.Header;
+
+public class TransactionsController {
+    public Header createTransaction = ctx ->{
+        ctx.result(1, 1, 1, 3.00, 1);
+        ctx.result(200);
+    };
+
+    public Header getAllTransactionByCustomerId = ctx->{
+        ctx.result(1, 1, 1, 3.00, 1);
+        ctx.result(200);
+    };
+
+    public Header getAllTransactionsByProducerId = ctx->{
+        ctx.result(1, 1, 1, 3.00, 10);
+        ctx.result(200);
+    };
+}
