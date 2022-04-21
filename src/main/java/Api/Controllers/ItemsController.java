9c375c0f5d5b129778f8bf883454c4788fbab650
William Blair <51091296+Skocious@users.noreply.github.com>
2022-04-21 14:38:05 -0500

    Update ItemsController.java

--- src/main/java/Api/Controllers/ItemsController.java
+++ src/main/java/Api/Controllers/ItemsController.java
@@ -6,79 +6,80 @@
 //import entities.Items;
 //import io.javalin.core.util.Header;
 //import io.javalin.http.Handler;
-//
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
 //
 //
 //public class ItemsController {
-//    public ItemsSO itemsimp;
 //
-//    public ItemsController(ItemsSO itemsimp){
+//    public static Logger logger = LogManager.getLogger(ItemsController.class);
 //
-//        this.itemsimp = itemsimp;
-//    }
+//    public ItemsSO itemsSO;
+//
+//    public ItemsController(ItemsSO itemsSO) {
+//        this.itemsSO = itemsSO;
+//    };
 //
 //    public Handler createItems = ctx ->{
 //        String body = ctx.body();
 //        Gson gson = new Gson();
 //        Items items = gson.fromJson(body, Items.class);
-//        itemsimp.serviceCreateItems(items);
+//        logger.info("Attempting to add item to inventory");
+//        itemsSO.serviceCreateItems(items);
 //        ctx.result("Item added to inventory");
 //        ctx.status(201);
+//        logger.info("Item added to inventory");
 //    };
 //
-//    public Handler selectAllItemsById = ctx ->{};
+//    public Handler selectAllItems = ctx ->{
+//        String body = ctx.body();
+//        Gson gson = new Gson();
+//        Items items = gson.fromJson(body, Items.class);
+//        String jsonString = gson.toJson(itemsSO.serviceSelectAllItems());
+//        ctx.result(jsonString);
+//        ctx.status(200);
+//    };
 //
 //    public Handler updateItemsById = ctx ->{
 //        String body = ctx.body();
 //        Gson gson = new Gson();
 //        Items items = gson.fromJson(body, Items.class);
-//        for(Items i : items){
-//            if(i.getItem_id() == items.getItem_id()){
-//                int index = items.indexOf(i);
-//                items.set(index, items);
-//                String itemsJson = gson.toJson(items);
-//                ctx.result(itemsJson);
-//                ctx.status(200);
-//            }
-//        }
+//        itemsSO.serviceUpdateItemsById(items);
+//        ctx.result("Item updated");
+//        ctx.status(200);
 //    };
 //
 //    public Handler deleteItemsById = ctx ->{
-//        int id = Integer.parseInt(ctx.pathParam("id"));
-//        int index = -1;
-//        for(Items i: items){
-//            if(i.getItem_id() == id){
-//                index = items.indexOf(i);
-//            }
-//        }if(index != -1){
-//            items.remove(index);
-//            ctx.result("Item removed");
-//            ctx.status(200);
-//        }
+//        String body = ctx.body();
+//        Gson gson = new Gson();
+//        Items items = gson.fromJson(body, Items.class);
+//        itemsSO.serviceDeleteItemsById(items.getItem_id());
+//        ctx.result("Item deleted");
+//        ctx.status(200);
 //    };
 //
 //    public Header createItems = ctx ->{
 //        ctx.result(1, 1, "Milk", 3.00);
-//        ctx.result(200);
+//        ctx.status(200);
 //    };
 //
 //    public Header selectItemsById = ctx ->{
 //        ctx.result(4, 1, "goat", "brown goat", 99.99);
-//        ctx.result(200);
+//        ctx.status(200);
 //    };
 //
 //    public Header selectAllItems = ctx ->{
 //        ctx.result(ItemsImp.itemsList);
-//        ctx.result(200);
+//        ctx.status(200);
 //    };
 //
 //    public Header updateItemsById = ctx ->{
 //        ctx.result(4, 1, "eggs", "dozen large grade eggs", 6.00);
-//        ctx.result(200);
+//        ctx.status(200);
 //    };
 //
 //    public Header deleteItemsById = ctx ->{
 //        ctx.result("Item deleted");
-//        ctx.result(200);
+//        ctx.status(200);
 //    };
 //}
