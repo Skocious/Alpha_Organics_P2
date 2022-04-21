c7756edd9ade74cdbb40c077237ccf8a2906f66d
William Blair <51091296+Skocious@users.noreply.github.com>
2022-04-21 14:38:35 -0500

    Update ProducerControllers.java

--- src/main/java/Api/Controllers/ProducerControllers.java
+++ src/main/java/Api/Controllers/ProducerControllers.java
@@ -1,9 +1,25 @@
 //package Api.Controllers;
 //
+//import ServiceAccessLayer.*;
+//import com.google.gson.Gson;
 //import entities.Producer;
 //import io.javalin.core.util.Header;
+//import io.javalin.http.Handler;
 //
 //public class ProducerControllers {
+//    public ProducerSO producerSO;
+//    public ProducerControllers(ProducerSO producerSO {
+//        this.producerSO = producerSO;
+//    };
+//
+//    public Handler getProducerId = ctx ->{
+//        String body = ctx.body();
+//        Gson gson = new Gson();
+//        Producer producer = gson.fromJson(body, Producer.class);
+//        ProducerSImp.serviceSelectProducerId();
+//        ctx.result();
+//        ctx.status(200);
+//    };
 //
 //    public Header getProducerId =ctx ->{
 //        ctx.result(Producer);
