ab7574fc92790122a3f8a742f5e2e767aac530e4
dankuczynski <dankuczynski@gmail.com>
2022-04-22 16:14:24 -0400

    commented out api

--- src/main/java/Api/Apps.java
+++ src/main/java/Api/Apps.java
@@ -1,47 +1,47 @@
-package Api;
-
-import Api.Controllers.*;
-import DataAccessLayer.*;
-import ServiceAccessLayer.*;
-import io.javalin.Javalin;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public class Apps {
-
-    public static Logger logger = LogManager.getLogger(Apps.class);
-
-    public static void main(String[] args){
-        Javalin app = Javalin.create(config ->{
-            config.enableCorsForAllOrigins();
-            config.enableDevLogging();
-        });
-
-        LoginDAO loginDAO = new LoginImp();
-        LoginSO loginSO = new LoginSImp(loginDAO);
-        LoginControllers loginControllers = new LoginControllers(loginSO);
-
-        ItemsImp itemsDAO = new ItemsImp();
-        ItemsSO itemsSO = new ItemsSImp(itemsDAO);
-        ItemsController itemsController = new ItemsController(itemsSO);
-
-        TransactionDAO transactionDAO = new TransactionImp();
-        TransactionSO transactionsSO = new TransactionSImp(transactionDAO);
-        TransactionsController transactionsController = new TransactionsController(transactionsSO);
-
-        app.post("/items", itemsController.createItems);
-
-        app.get("/items", itemsController.selectAllItems);
-
-        app.put("/items", itemsController.updateItemsById);
-
-        app.delete("/items/{id}", itemsController.deleteItemsById);
-
-        app.post("/customer", loginControllers.selectLoginName);
-
-        app.post("/transactions", transactionsController.createTransaction);
-
-        app.get("/transactions/{producerId}", transactionsController.getAllTransactionByUserName);
-
-    }
-}
+//package Api;
+//
+//import Api.Controllers.*;
+//import DataAccessLayer.*;
+//import ServiceAccessLayer.*;
+//import io.javalin.Javalin;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+//
+//public class Apps {
+//
+//    public static Logger logger = LogManager.getLogger(Apps.class);
+//
+//    public static void main(String[] args){
+//        Javalin app = Javalin.create(config ->{
+//            config.enableCorsForAllOrigins();
+//            config.enableDevLogging();
+//        });
+//
+//        LoginDAO loginDAO = new LoginImp();
+//        LoginSO loginSO = new LoginSImp(loginDAO);
+//        LoginControllers loginControllers = new LoginControllers(loginSO);
+//
+//        ItemsImp itemsDAO = new ItemsImp();
+//        ItemsSO itemsSO = new ItemsSImp(itemsDAO);
+//        ItemsController itemsController = new ItemsController(itemsSO);
+//
+//        TransactionDAO transactionDAO = new TransactionImp();
+//        TransactionSO transactionsSO = new TransactionSImp(transactionDAO);
+//        TransactionsController transactionsController = new TransactionsController(transactionsSO);
+//
+//        app.post("/items", itemsController.createItems);
+//
+//        app.get("/items", itemsController.selectAllItems);
+//
+//        app.put("/items", itemsController.updateItemsById);
+//
+//        app.delete("/items/{id}", itemsController.deleteItemsById);
+//
+//        app.post("/customer", loginControllers.selectLoginName);
+//
+//        app.post("/transactions", transactionsController.createTransaction);
+//
+//        app.get("/transactions/{producerId}", transactionsController.getAllTransactionByUserName);
+//
+//    }
+//}
