510ea345859a5f344a892ba584d784336e4078f8
William Blair <blaircorporate@gmail.com>
2022-04-25 14:49:22 -0500

    Commented out LoginController

--- src/main/java/Api/Controllers/LoginControllers.java
+++ src/main/java/Api/Controllers/LoginControllers.java
@@ -1,33 +1,33 @@
-package Api.Controllers;
-
-import DataAccessLayer.*;
-import ServiceAccessLayer.*;
-import com.google.gson.Gson;
-import entities.*;
-import io.javalin.core.util.Header;
-import io.javalin.http.Handler;
-
-import javax.security.auth.login.FailedLoginException;
-
-public class LoginControllers {
-    public LoginSO loginSO;
-    public LoginControllers(LoginSO loginSO){
-        this.loginSO = loginSO;
-    };
-
-    public Handler selectLoginName = ctx ->{
-        String body = ctx.body();
-        Gson gson = new Gson();
-        Login login = gson.fromJson(body, Login.class); {
-            loginSO.serviceSelectLoginName(login.getUsername(), login.getPassword());
-            ctx.result(login.getUsername()).status();
-            ctx.status(201);
-            ctx.status(400);
-    };
-
-
-    public Header selectLoginName = ctx ->{
-        ctx.result(LoginImp.Login);
-        ctx.status(200);
-    };
-};
+//package Api.Controllers;
+//
+//import DataAccessLayer.*;
+//import ServiceAccessLayer.*;
+//import com.google.gson.Gson;
+//import entities.*;
+//import io.javalin.core.util.Header;
+//import io.javalin.http.Handler;
+//
+//import javax.security.auth.login.FailedLoginException;
+//
+//public class LoginControllers {
+//    public LoginSO loginSO;
+//    public LoginControllers(LoginSO loginSO){
+//        this.loginSO = loginSO;
+//    };
+//
+//    public Handler selectLoginName = ctx ->{
+//        String body = ctx.body();
+//        Gson gson = new Gson();
+//        Login login = gson.fromJson(body, Login.class); {
+//            loginSO.serviceSelectLoginName(login.getUsername(), login.getPassword());
+//            ctx.result(login.getUsername()).status();
+//            ctx.status(201);
+//            ctx.status(400);
+//    };
+//
+//
+//    public Header selectLoginName = ctx ->{
+//        ctx.result(LoginImp.Login);
+//        ctx.status(200);
+//    };
+//};
