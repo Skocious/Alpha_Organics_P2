969e4a078e87cb06b5c8ead082df522b0e36f53d
dankuczynski <dankuczynski@gmail.com>
2022-04-26 17:16:15 -0400

    All API elements should be working

--- src/main/java/Api/Controllers/TransactionsController.java
+++ src/main/java/Api/Controllers/TransactionsController.java
@@ -1,11 +1,13 @@
 package Api.Controllers;
 
-import DataAccessLayer.*;
 import ServiceAccessLayer.*;
 import com.google.gson.Gson;
 import entities.*;
 import io.javalin.core.util.Header;
 import io.javalin.http.Handler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TransactionsController {
     public TransactionSO transactionSO;
@@ -28,12 +30,15 @@ public class TransactionsController {
         String body = ctx.body();
         Gson gson = new Gson();
         Transaction transaction = gson.fromJson(body, Transaction.class);
-        String jsonString = gson.toJson(transactionSO.serviceGetAllTransactionByUsername(transaction.getUsername()));
-        ctx.result(jsonString);
+        List<Transaction> transactions = transactionSO.serviceGetAllTransactionByUsername(transaction.getUsername());
+        List<String> jsonTransaction = new ArrayList<>();
+        for(Transaction t: transactions){
+        String json = gson.toJson(t);
+        jsonTransaction.add(json);}
+        ctx.result(jsonTransaction.get(transaction.getTransactionId()));
         ctx.status(200);
     };
-
-
+    
 //    public Header createTransaction = ctx ->{
 //        ctx.result(1, 1, 1, 3.00, 1);
 //        ctx.status(201);
